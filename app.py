c919aec2bc400496a8bf1677a05160f78f7273ed
jgregoriBb <javier.gregori@blackboard.com>
2022-09-29 17:18:50 +0100

    Multiple fixes

--- app.py
+++ app.py
@@ -2,31 +2,47 @@ from alive_progress import alive_it
 from script_helper import Downloader
 from script_helper import Tools
 import os
+import csv
 
 
 def main():
     downloader = Downloader()
     tools = Tools()
+    original_courses = tools.read_from_csv('original', './input_files/original.csv')
+    ultra_courses = tools.read_from_csv('ultra','./input_files/ultra_course_id.csv')
+
     tools.create_folder('./sa_submissions')
+    
+
+    print('[DOWNLOADER] Starting script')
     print('[DOWNLOADER] Processing Original courses.')
-    original_courses = downloader.get_courses_original()
+   
+    # Get course list from CSV
+    
+    print(original_courses)
     print(
-        f'[DOWNLOADER] {len(original_courses)} Original courses found in the system.')
+       f'[DOWNLOADER] {len(original_courses)} Original courses found in the system.')
     for c in alive_it(original_courses):
+        downloader.authenticate()
         content_areas = downloader.get_top_content_original(c['id'])
         if content_areas:
             print('[DOWNLOADER] Retrieving content areas.')
             for ca in content_areas:
+                downloader.authenticate()
                 assessments = downloader.get_sa_assessments_original(
                     c['id'], ca['id'])
                 if assessments:
                     print('[DOWNLOADER] Retrieving asessments.')
                     for a in assessments:
+                        print(f'[DOWNLOADER] Assessment {a["id"]} ')
+                        downloader.authenticate()
                         attempts = downloader.get_attempts(
                             c['id'], a['columnId'])
                         if attempts:
                             print('[DOWNLOADER] Retrieving attempts.')
                             for at in attempts:
+                                print(f'[DOWNLOADER] Attempt {at["id"]} ')
+                                downloader.authenticate()
                                 metadata = downloader.get_file_metadata(
                                     c['id'], at['id'])
                                 if metadata:
@@ -42,7 +58,6 @@ def main():
                                     os.chdir('../..')
     print('[DOWNLOADER] safeAssign assignments download complete for original courses')
     print('[DOWNLOADER] Processing Ultra courses.')
-    ultra_courses = downloader.get_courses_ultra()
     print(
         f'[DOWNLOADER] {len(ultra_courses)} Ultra courses found in the system.')
     for c in alive_it(ultra_courses):
---
+++ csv_gen.py
@@ -0,0 +1,47 @@
+from script_helper import Downloader
+from script_helper import Tools
+import csv
+import os
+
+class Csv_Gen():
+
+    def __init__(self):
+        self.downloader = Downloader()
+        self.downloader.authenticate()
+
+    def generate_file_original(self):
+        self.original_courses = self.downloader.get_courses_original()
+        try:
+            with open('./input_files/original.csv','w',newline='') as csvfile:
+                self.fieldnames = ['course_id','ultraStatus','course_name']
+                self.writer = csv.DictWriter(csvfile, self.fieldnames)
+                self.writer.writeheader()
+                for self.c in self.original_courses:
+                    self.writer.writerow({'course_id':self.c["id"],'ultraStatus':self.c["ultraStatus"],'course_name':self.c["name"]})
+        except FileExistsError:
+            print('[DOWNLOADER][CSV_GEN] File already exists')
+
+    def generate_file_ultra(self):
+        self.ultra_courses = self.downloader.get_courses_ultra()
+        try:
+            with open('./input_files/ultra.csv','w',newline='') as csvfile:
+                self.fieldnames = ['course_id','ultraStatus','course_name']
+                self.writer = csv.DictWriter(csvfile, self.fieldnames)
+                self.writer.writeheader()
+                for self.c in self.ultra_courses:
+                    self.writer.writerow({'course_id':self.c["id"],'ultraStatus':self.c["ultraStatus"],'course_name':self.c["name"]})
+        except FileExistsError:
+            print('[DOWNLOADER][CSV_GEN] File already exists')
+
+
+
+if __name__ == "__main__":
+    file_gen = Csv_Gen()
+    tools = Tools()
+    
+    tools.create_folder('./input_files')
+    file_gen.generate_file_ultra()
+    file_gen.generate_file_original()
+    os.chdir('../')
+    
+   
